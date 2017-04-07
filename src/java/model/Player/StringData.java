f9a6294d4428d1a5542906034f954f55f9dfdc77
jedrickboca <jedrickboca@gmail.com>
2017-04-07 13:25:13 -0400

    Updated it to new fields from user table

--- src/java/model/Player/StringData.java
+++ src/java/model/Player/StringData.java
@@ -11,27 +11,35 @@ package model.Player;
  */
 public class StringData {
 
-    public String playerId = "";
-    public String playerName = "";
-    public String emailAddress = "";
-    public String pswd = "";
-    public String skillLevel = "";
-    public String roleName = "";
+    public String userId = "";
+    public String user_name = "";
+    public String email = "";
+    public String first_name = "";
+    public String last_name = "";
+    public String phone_number = "";
+    public String salt = "";
+    public String role_id = "";
+    public String password_id = "";
+    public String system_ip = "";
     public String errorMsg = ""; // this field is used, for example, to hold db error while attempting login.
 
     public int getCharacterCount(){
-        String S = this.playerId + this.playerName + this.emailAddress + this.pswd + this.skillLevel + this.roleName;
+        String S = this.userId + this.user_name + this.email + this.first_name + this.last_name + this.phone_number + this.salt + this.role_id + this.password_id + this.system_ip;
         
         return S.length();
     }
     
     public String toString() {
-        return "Player Id:" + this.playerId
-                + ", Player Name:" + this.playerName 
-                + ", Email Address:" + this.emailAddress
-                + ", Password:" + this.pswd
-                + ", Skill Level:" + this.skillLevel
-                + ", Role Name:" + this.roleName
+        return "Userr Id:" + this.userId
+                + ", user_name:" + this.user_name
+                + ", Email Address:" + this.email
+                + ", Frist Name:" + this.first_name
+                + ", Last Name:" + this.last_name
+                + ", Phone Number:" + this.phone_number
+                + ", Salt:" + this.salt
+                + ", Role Id:" + this.role_id
+                + ", Password Id:" + this.password_id
+                + ", System IP;:" + this.system_ip
                 + ", errorMsg:" + this.errorMsg;
     }
     
